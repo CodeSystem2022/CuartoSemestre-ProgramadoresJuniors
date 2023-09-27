fe7e6e7a5e7d0c177e02ade2aa81782a4cf525e8
EmirMaya <emirmayaok@gmail.com>
2023-09-27 18:01:44 -0300

    agregando controllers

--- LaboratorioIV/JavaScript4/PERN-STACK/src/app.js
+++ LaboratorioIV/JavaScript4/PERN-STACK/src/app.js
@@ -1,5 +1,7 @@
 import morgan from "morgan";
 import express from 'express';
+import tareasRoutes from './router/tareas.routes.js';
+import authRoutes from './router/auth.routes.js';
 
 const app = express();
 
@@ -8,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.json({ message: "Hello, welcome to my ecommerce" }));
+app.use('/api', tareasRoutes);
+app.use('/api', authRoutes);
 
 app.use((err, req, res, next) => {
     res.status(500).json({
