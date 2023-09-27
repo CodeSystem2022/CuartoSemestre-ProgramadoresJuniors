06e59836a2390c6841f4b4926e3efbb49a635072
EmirMaya <emirmayaok@gmail.com>
2023-09-27 09:39:47 -0300

    python

--- LaboratorioIV/Python4/Clase01/doc_str.py
+++ LaboratorioIV/Python4/Clase01/doc_str.py
@@ -13,5 +13,6 @@ from mi_clase import MiClase
 # print(MiClase.__doc__)
 # print(MiClase.__init__.__doc__)
 # print(MiClase.mi_metodo.__doc__)
+#good
 print(MiClase.mi_metodo)
 print(type(MiClase.mi_metodo))
