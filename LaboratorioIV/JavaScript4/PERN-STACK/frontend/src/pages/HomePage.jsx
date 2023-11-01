3291884562f99dabb9b2169693c7178c130cc7c9
biancatosetto <biancatosetto@gmail.com>
2023-11-01 20:34:33 -0300

    PERN STACK
    About Page y Home Page

--- LaboratorioIV/JavaScript4/PERN-STACK/frontend/src/pages/HomePage.jsx
+++ LaboratorioIV/JavaScript4/PERN-STACK/frontend/src/pages/HomePage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Card } from "../components/ui";
 
 function HomePage() {
+  const data = useContext(AuthContext);
+  
   return (
-    <div>HomePage</div>
-  )
+    <Card>
+      <h1 className="font-bold justify-center text-2xl py-4">
+        {" "}
+        Proyect Stack PERN
+      </h1>
+      <h3>
+        El stack PERN (PostgreSQL, Express, React y Node.js) se ha convertido en
+        una opción popular para el desarrollo de aplicaciones web de alta calidad. 
+        En este artículo exploraremos cómo crear una aplicación PERN que incluye 
+        la autenticación de usuarios y operaciones CRUD (Crear, Leer, Actualizar, 
+        Eliminar) tareas. Este proyecto versátil es ideal para una amplia gama de 
+        aplicaciones, desde listas de tareas personales hasta aplicaciones de gestión 
+        de proyectos.
+      </h3>
+    </Card>
+  );
 }
 
-export default HomePage
+export default HomePage;
