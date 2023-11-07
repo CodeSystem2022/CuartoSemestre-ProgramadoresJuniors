02a96cb04b87462eb5d1cc0e761d99259a56552b
EmirMaya <emirmayaok@gmail.com>
2023-11-07 11:25:59 -0300

    clase 11 python matplotlib graficas paises america del sur

--- LaboratorioIV/Python4/python_avanzado/app/charts.py
+++ LaboratorioIV/Python4/python_avanzado/app/charts.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
 
+
 def generate_bar_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
+  plt.savefig(f'./imgs/{name}.png')
   plt.show()
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
+  plt.savefig('bar.png')
   plt.show()
 
 if __name__ == '__main__':
--- LaboratorioIV/Python4/python_avanzado/app/main.py
+++ LaboratorioIV/Python4/python_avanzado/app/main.py
@@ -3,13 +3,13 @@ import read_csv
 import charts
 
 def run():
-  data = read_csv.read_csv('./app/data.csv') # leemos el csv, gracias al módulo línea 2
+  data = read_csv.read_csv('./data.csv') # leemos el csv, gracias al módulo línea 2
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
-  '''
+
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
@@ -17,7 +17,7 @@ def run():
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
-'''
+    charts.generate_bar_chart(country['Country'], labels, values)
+
 if __name__ == '__main__':
   run()
